690d2fbaadf58be98b28ae1fb9d4d35cd8a9d9b3
Aniket Pandit <aniketypandit99@gmail.com>
2025-05-19 00:23:25 +0530

    Update code little bit

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -58,6 +58,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
+      sameSite: "None",
       maxAge: 1000 * 60 * 60 * 24,
     });
     res.send({
