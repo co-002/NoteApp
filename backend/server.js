b6ad82e873436e902debc1820c9c4eaabef602f3
Aniket Pandit <aniketypandit99@gmail.com>
2025-05-19 00:06:27 +0530

    Added deployed url's

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,6 @@ import noteRouter from "./routes/note.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
-
-//     https://chatgpt.com/c/68282e1b-9654-8002-9154-2b75830369a7
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -17,7 +15,7 @@ connectDB();
 // Middlewares
 dotenv.config();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://noteapp-w3u7.onrender.com",
   methods: ["GET", "PUT", "POST", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type"]
